06954670352f4948a366e0ad2a3510dfd280a0b3
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-08-24 20:29:40 -0400

    initial commit

---
+++ mybot.js
@@ -0,0 +1,20 @@
+// Slack API Token - xoxb-72751703124-nwFbO4LEOloSp7Ryb2YZHOhq
+
+var SlackAPIKey = 'xoxb-72751703124-nwFbO4LEOloSp7Ryb2YZHOhq';
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+  debug: false
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: SlackAPIKey,
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+
+  bot.reply(message,'Hello yourself.');
+
+});
