fbb985c606c75064cab2702471ec808ca3008ed4
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-08-28 18:14:34 -0400

    testing something

--- mybot.js
+++ mybot.js
@@ -24,3 +24,8 @@ controller.hears('Lunchables?',['direct_message','direct_mention','mention'],fun
   bot.reply(message,'Why sure! I love luncahbles!');
 
 });
+
+var http = require('http'); 
+http.createServer(function (req, res) { 
+    res.writeHead(200, {'Content-Type': 'text/plain'}); 
+    res.end('Hello World\n');  }).listen(process.env.PORT || 5000); 
