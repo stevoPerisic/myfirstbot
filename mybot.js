28b3b4e548a7a8e6fc7a3f51d9ef9db89cccabdb
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-08-29 21:26:45 -0400

    FB bot addition

--- mybot.js
+++ mybot.js
@@ -30,16 +30,6 @@ controller.hears('Lunchables?',['direct_message','direct_mention','mention'],fun
 
 });
 
-// var http = require('http'); 
-// http.createServer(function (req, res) { 
-//     res.writeHead(200, {'Content-Type': 'text/plain'}); 
-//     res.end('Hello World\n');  
-// }).listen(process.env.PORT || 5000);
-
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello world');
@@ -49,6 +39,7 @@ app.listen((process.env.PORT || 5000), function () {
   console.log('Listening on port 5000');
 });
 
+/*** FACEBOOK BOT ********/
 // respond to facebook's verification
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === token) {
@@ -58,40 +49,77 @@ app.get('/webhook/', function (req, res) {
   }
 });
 
-// respond to post calls from facebook
-app.post('/webhook/', function (req, res) {
-	console.log(req);
-  var messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i];
-    var sender = event.sender.id;
-    if (event.message && event.message.text) {
-      var incomingText = event.message.text;
-      console.log('You sent the message', incomingText);
-      sendTextMessage(sender, "Text received, echo: "+ incomingText.substring(0, 200));
-    }
-  }
-  res.sendStatus(200);
+var controllerFB = Botkit.facebookbot({
+    debug: true,
+    access_token: process.env.FB_PAGE_ACCESS_TOKEN,
+    verify_token: process.env.FB_VERIFY_TOKEN,
+});
+var bot = controllerFB.spawn({
+});
+controllerFB.setupWebserver(process.env.port || 3000, function(err, webserver) {
+    controllerFB.createWebhookEndpoints(webserver, bot, function() {
+        console.log('ONLINE!');
+        // if(ops.lt) {
+            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+                if (err) {
+                    console.log(err);
+                    process.exit();
+                }
+                console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
+            });
+
+            tunnel.on('close', function() {
+                console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
+                process.exit();
+            });
+        //}
+    });
 });
 
-function sendTextMessage(sender, text) {
-  var access_token = process.env.FB_PAGE_ACCESS_TOKEN;
-  var messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:access_token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
+controllerFB.hears(['hello', 'hi'], 'message_received', function(bot, message) {
+    controllerFB.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
+
+// respond to post calls from facebook
+// app.post('/webhook/', function (req, res) {
+// 	console.log(req);
+//   var messaging_events = req.body.entry[0].messaging;
+//   for (i = 0; i < messaging_events.length; i++) {
+//     var event = req.body.entry[0].messaging[i];
+//     var sender = event.sender.id;
+//     if (event.message && event.message.text) {
+//       var incomingText = event.message.text;
+//       console.log('You sent the message', incomingText);
+//       sendTextMessage(sender, "Text received, echo: "+ incomingText.substring(0, 200));
+//     }
+//   }
+//   res.sendStatus(200);
+// });
+
+// function sendTextMessage(sender, text) {
+//   var access_token = process.env.FB_PAGE_ACCESS_TOKEN;
+//   var messageData = {
+//     text:text
+//   }
+//   request({
+//     url: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: {access_token:access_token},
+//     method: 'POST',
+//     json: {
+//       recipient: {id:sender},
+//       message: messageData,
+//     }
+//   }, function(error, response, body) {
+//     if (error) {
+//       console.log('Error sending message: ', error);
+//     } else if (response.body.error) {
+//       console.log('Error: ', response.body.error);
+//     }
+//   });
+// }
