27027a89ff8e006a7fa5dbda531563a5c81a3d9d
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-08-29 21:29:34 -0400

    commented out localtunnel

--- mybot.js
+++ mybot.js
@@ -56,25 +56,26 @@ var controllerFB = Botkit.facebookbot({
 });
 var bot = controllerFB.spawn({
 });
-controllerFB.setupWebserver(process.env.port || 3000, function(err, webserver) {
-    controllerFB.createWebhookEndpoints(webserver, bot, function() {
-        console.log('ONLINE!');
-        // if(ops.lt) {
-            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
-                if (err) {
-                    console.log(err);
-                    process.exit();
-                }
-                console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
-            });
 
-            tunnel.on('close', function() {
-                console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
-                process.exit();
-            });
-        //}
-    });
-});
+// controllerFB.setupWebserver(process.env.port || 5000, function(err, webserver) {
+//     controllerFB.createWebhookEndpoints(webserver, bot, function() {
+//         console.log('ONLINE!');
+//         // if(ops.lt) {
+//             var tunnel = localtunnel(process.env.port || 5000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+//                 if (err) {
+//                     console.log(err);
+//                     process.exit();
+//                 }
+//                 console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
+//             });
+
+//             tunnel.on('close', function() {
+//                 console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
+//                 process.exit();
+//             });
+//         //}
+//     });
+// });
 
 controllerFB.hears(['hello', 'hi'], 'message_received', function(bot, message) {
     controllerFB.storage.users.get(message.user, function(err, user) {
