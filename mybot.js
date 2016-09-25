5084f0f95dca1547cb94963b67731553650f11f1
Stevo Perisic <stevo.perisic@maestralsolutions.com>
2016-09-25 11:48:33 -0400

    back to attachments, quick replies are not working for some reason

--- mybot.js
+++ mybot.js
@@ -227,20 +227,7 @@ var storage = require('./lib/mongoStorage');
 
 		if(message.payload === 'Get started'){
 			bot.reply(message, {
-				"text":"Pick a color:",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Red",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-      },
-      {
-        "content_type":"text",
-        "title":"Green",
-        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-      }
-    ]
-		    	/*"attachment":{
+		    	"attachment":{
 		    		"type":"template",
 					"payload":{
 						"template_type": "button",
@@ -258,7 +245,7 @@ var storage = require('./lib/mongoStorage');
 							}
 						]
 					}
-				}*/
+				}
 			});
 		} else if(message.payload.surveyAnswer) {
 			bot.reply(message, message.payload.surveyAnswer)
